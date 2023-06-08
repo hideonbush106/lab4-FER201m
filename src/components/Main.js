f340345b2861b0b61594b09b9562835c5455899c
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-06-08 16:58:27 +0700

    remove proptype

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
@@ -31,10 +30,5 @@ function Main(props) {
         </Grid>
     );
 }
-
-Main.propTypes = {
-    posts: PropTypes.arrayOf(PropTypes.string).isRequired,
-    title: PropTypes.string.isRequired,
-};
 
 export default Main;
--- src/components/MainNewsFeature.js
+++ src/components/MainNewsFeature.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -59,14 +58,5 @@ function MainNewsFeature(props) {
     );
 }
 
-MainNewsFeature.propTypes = {
-    post: PropTypes.shape({
-        description: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
-        imageText: PropTypes.string.isRequired,
-        linkText: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-    }).isRequired,
-};
 
 export default MainNewsFeature;
